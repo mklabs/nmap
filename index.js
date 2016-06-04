1940ce89660a272d59b864984c7d413ef4897e8b
Hemanth.HM <hemanth.hm@gmail.com>
2016-06-04 19:25:53 +0530

    init0

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const bindings = require('./bindings.json');
+const keymaster = require('keymaster');
+
+module.exports = function (str, opts) {
+	const bindingKeys = Object.keys(bindings);
+	// TODO #1
+};
